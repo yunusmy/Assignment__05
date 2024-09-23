c00e54f39725876c436cbfae814dab3db6246feb
yunusmy <yunusmd804@gmail.com>
2024-09-23 21:02:31 +0600

    donaed for donate for Flood at Noakhali

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ donateNowButton.addEventListener('click', function () {
   const mainSection = document.getElementById('main-section');
 
   mainSection.classList.remove('hidden');
+  donateNowButton.classList.add('bg-btnColor')
+
+  const historyButton = document.getElementById('history-button');
+  historyButton.classList.remove('bg-btnColor')
 
   const historySection = document.getElementById('history-section');
   historySection.classList.add('hidden');
@@ -26,10 +30,13 @@ const historyButton = document.getElementById('history-button');
 
 historyButton.addEventListener('click', function () {
   const mainSection = document.getElementById('main-section');
-
+  historyButton.classList.add('bg-btnColor')
+  const donateNowButton = document.getElementById('donnate-now-button');
+donateNowButton.classList.remove('bg-btnColor')
   mainSection.classList.add('hidden');
 
-const historySection = document.getElementById('history-section');
+
+  const historySection = document.getElementById('history-section');
   historySection.classList.remove('hidden');
 
 
@@ -53,6 +60,8 @@ donateButton.addEventListener('click', function (event) {
   let donationAmounts = parseFloat(donationAmount);
   let totalAmounts = parseFloat(totalAmount);
 
+  let noakhaliNow = document.getElementById ('Noakhali');
+
 
   if (!isNaN(inputTakas) && inputTakas > 0 && inputTakas < totalAmounts) {
   let totalAmountBox = totalAmounts - inputTakas;
@@ -62,6 +71,14 @@ donateButton.addEventListener('click', function (event) {
   let donationAmountBox = inputTakas + donationAmounts;
 
     document.getElementById('donation-amount').innerText = donationAmountBox.toFixed(2);
+
+    noakhaliNow.innerHTML = ` 
+        <p class="text-xs text-gray-500">${inputTakas}  Taka is Donated for Donate for Flood at Noakhali, Bangladesh.
+      </p>
+        <p class="text-xs text-gray-500">Data: ${new Date().toLocaleDateString()}</p>
+        
+    
+    `;
     
     document.getElementById('input-taka').value = '';
 
@@ -88,6 +105,9 @@ donateButton2.addEventListener('click', function (e) {
   let donationAmounts2 = parseFloat(donationAmount2);
   let totalAmounts2 = parseFloat(totalAmount2);
 
+  let noakhaliNow2 = document.getElementById ('Noakhali2');
+
+
 
   if (!isNaN(inputTakas2) && inputTakas2 > 0 && inputTakas2 < totalAmounts2) {
   
@@ -100,6 +120,16 @@ donateButton2.addEventListener('click', function (e) {
   let donationAmountBox2 = inputTakas2 + donationAmounts2;
 
     document.getElementById('donation-amount2').innerText = donationAmountBox2;
+
+    noakhaliNow2.innerHTML = ` 
+        <p class="text-xs text-gray-500">${inputTakas2}  Taka is Donated for Donate for Flood at Noakhali, Bangladesh.
+      </p>
+        <p class="text-xs text-gray-500">Data: ${new Date().toLocaleDateString()}</p>
+        
+    
+    `;
+    
+
     
      document.getElementById('input-taka2').value = '';
 
@@ -116,6 +146,7 @@ donateButton2.addEventListener('click', function (e) {
 
 })
 
+
 const donateButton3 = document.getElementById('donate-button3');
 
 donateButton3.addEventListener('click', function (en) {
@@ -129,6 +160,9 @@ donateButton3.addEventListener('click', function (en) {
   let donationAmounts3 = parseFloat(donationAmount3);
   let totalAmounts3 = parseFloat(totalAmount3);
 
+    let noakhaliNow3 = document.getElementById ('Noakhali3');
+
+
 
   if (!isNaN(inputTakas3) && inputTakas3 > 0 && inputTakas3 < totalAmounts3) {
   
@@ -141,6 +175,17 @@ donateButton3.addEventListener('click', function (en) {
   let donationAmountBox3 = inputTakas3 + donationAmounts3;
 
     document.getElementById('donation-amount3').innerText = donationAmountBox3;
+
+
+
+    noakhaliNow3.innerHTML = ` 
+        <p class="text-xs text-gray-500">${inputTakas3}  Taka is Donated for Donate for Flood at Noakhali, Bangladesh.
+      </p>
+        <p class="text-xs text-gray-500">Data: ${new Date().toLocaleDateString()}</p>
+        
+    
+    `;
+    
     
      document.getElementById('input-taka3').value = '';
 
